f43660f54be883e6b7b5945e16ff587f10dab0db
dheeraj <dheeraj.thodupuynoori01@gmail.com>
2019-07-20 04:47:51 -0700

    added authors divisions but not synchronized

---
+++ Week3/js/GenerateAuthorsDivision.js
@@ -0,0 +1,33 @@
+export default function createAuthorsDivision(authors) {
+  console.log(authors);
+  let authorsDiv = document.getElementById("authors-division");
+  authors.forEach(author => {
+    let newsFeed = document.createElement("div");
+    newsFeed.setAttribute("class", "flex-container");
+    newsFeed.setAttribute("id", author.author);
+    let logo = document.createElement("div");
+    logo.setAttribute("class", "logo");
+    let logoImage = document.createElement("img");
+    logoImage.setAttribute("src", author.urlToImage);
+    logoImage.setAttribute("class", "logo-image");
+    logo.appendChild(logoImage);
+    let content = document.createElement("div");
+    content.setAttribute("class", "content");
+    let title = document.createElement("h3");
+    title.innerText = author.title;
+    let description = document.createElement("p");
+    description.innerText = author.description;
+    let clickToReadMore = document.createElement("button");
+    clickToReadMore.setAttribute("value", "Continue Reading");
+    clickToReadMore.setAttribute("class", "continue-reading");
+    // clickToReadMore.setAttribute("id", i);
+    // clickToReadMore.addEventListener("click", createPopUp);
+    clickToReadMore.innerText = "Continue Reading";
+    content.appendChild(title);
+    content.appendChild(description);
+    content.appendChild(clickToReadMore);
+    newsFeed.appendChild(logo);
+    newsFeed.appendChild(content);
+    authorsDiv.appendChild(newsFeed);
+  });
+}
--- Week3/js/HomeTask3.js
+++ Week3/js/HomeTask3.js
@@ -1,13 +1,30 @@
-import { newsApiEndPoint } from "./Endpoints.js";
+// import { newsApiEndPoint } from "./Endpoints.js";
 import getChannels from "./GetListOfChannels.js";
+import createChannelDropdown from "./GenerateDropdown.js";
+import getAuthorsList from "./GetAuthors.js";
+import createAuthorsDivision from "./GenerateAuthorsDivision.js";
 
 var channels = getChannels();
-// var data = Promise.resolve(channels).then(result => result);
-// console.log(channels);
-var result;
-console.log(
-  channels.then(data => {
-    result = data;
-  })
-);
-console.log(result);
+var authors;
+channels.then(data => {
+  createChannelDropdown(data);
+});
+var getAuthors = function() {
+  let selectedChannel = document.getElementById("select-channel");
+  let channelId = selectedChannel.value;
+  authors = getAuthorsList(channelId);
+  //   console.log(authors);
+  authors.then(data => {
+    // console.log(data);
+    createAuthorsDivision(data);
+  });
+};
+// writing this function because this js mentioned as type module so cant use "onchange" attributeon select in html
+// https://stackoverflow.com/questions/53069695/cant-call-a-function-from-js-file-imported-as-type-module
+// https://stackoverflow.com/questions/53630310/use-functions-defined-in-es6-module-directly-in-html
+function addOnChangedEventListenerToDropDown() {
+  var selectedChannel = document.getElementById("select-channel");
+  selectedChannel.addEventListener("change", getAuthors);
+  //   console.log("added event listener");
+}
+addOnChangedEventListenerToDropDown();
