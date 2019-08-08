873baf1ba0ea4d86b0c828390aa2359829b21ad5
dheeraj <dheeraj.thodupunoori01@gmail.com>
2019-08-08 19:25:16 +0530

    able to run application in edge , firefox except IE , need to work on it (lazy loading is pending)

--- Week5/webpack.config.js
+++ Week5/webpack.config.js
@@ -1,9 +1,26 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
 module.exports = {
   mode: "development",
   entry: "./js/HomeTask3.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  target: "node",
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   }
 };
