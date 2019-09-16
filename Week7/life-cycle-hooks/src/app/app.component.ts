08ae526907e010ff9b47401f280d63cfb94e4339
dheeraj <dheeraj.thodupunoori01@gmail.com>
2019-09-16 13:31:17 +0530

    implemented ngOnChanges and ngOnInit , implementing ngDoCheck

---
+++ Week7/life-cycle-hooks/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { User } from "./user";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "life-cycle-hooks";
+  userText: string = "Angular Task-7";
+  user: User;
+}
---
+++ Week7/life-cycle-hooks/src/app/test/test.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck
+} from "@angular/core";
+import { User } from "../user";
+
+@Component({
+  selector: "app-test",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.css"]
+})
+export class TestComponent implements OnInit, OnChanges, DoCheck {
+  @Input() public testInput: String;
+  @Input() public user: User;
+  constructor() {}
+
+  ngOnInit() {}
+  //Called before ngOnIt()
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("Changes", changes);
+    for (let propertyName in changes) {
+      let change = changes[propertyName];
+      let current = JSON.stringify(change.currentValue);
+      let previous = JSON.stringify(change.previousValue);
+      console.log(
+        propertyName,
+        ": Current Value :",
+        current,
+        "Previous Value :",
+        previous
+      );
+    }
+  }
+
+  ngDoCheck() {}
+}
