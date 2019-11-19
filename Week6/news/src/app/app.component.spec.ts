94232b332e143a81497f7572f5bfb54883434ca4
dheeraj <dheeraj.thodupunoori01@gmail.com>
2019-11-19 11:47:06 +0530

    added test cases to AppComponent

--- Week6/news/src/app/app.component.spec.ts
+++ Week6/news/src/app/app.component.spec.ts
@@ -1,33 +1,35 @@
 import { TestBed, async } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
+import { AuthComponent } from "./auth/auth.component";
+import { FooterComponent } from "./footer/footer.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-describe("AppComponent", () => {
+fdescribe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      imports: [RouterTestingModule, HttpClientModule, FormsModule],
+      declarations: [AppComponent, AuthComponent, FooterComponent]
     }).compileComponents();
   }));
 
-  // it("should create the app", () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-  // it(`should have as title 'news'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual("News");
-  // });
+  it(`should have as title 'news'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual("news");
+  });
 
-  // it("should render title", () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector(".content span").textContent).toContain(
-  //     "news app is running!"
-  //   );
-  // });
+  it("should have the other component", async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.componentInstance;
+    expect(compiled.querySelector("app-auth")).not.toBe(null);
+  }));
 });
