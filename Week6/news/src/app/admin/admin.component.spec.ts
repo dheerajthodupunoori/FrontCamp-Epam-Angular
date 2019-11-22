26f1bee47558121a8c41cc47c8ac4ae804936926
dheeraj <dheeraj.thodupunoori01@gmail.com>
2019-11-22 11:30:32 +0530

    added test cases to admin component

--- Week6/news/src/app/admin/admin.component.spec.ts
+++ Week6/news/src/app/admin/admin.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { users } from "../Config/user-mock-data";
 import { AdminComponent } from "./admin.component";
+import { UserService } from "../services/user.service";
+import { DebugElement } from "@angular/core";
 
-describe("AdminComponent", () => {
+fdescribe("AdminComponent", () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
+  let _userService: UserService;
+  let debugElement: DebugElement;
+  let _userServiceSpy;
+  let _userServiceFakeSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AdminComponent]
+      declarations: [AdminComponent],
+      providers: [UserService]
     }).compileComponents();
   }));
 
@@ -16,9 +23,31 @@ describe("AdminComponent", () => {
     fixture = TestBed.createComponent(AdminComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement;
+    _userService = debugElement.injector.get(UserService);
+    _userServiceSpy = spyOn(_userService, "getAllUsers").and.returnValue(users);
+    // _userServiceSpy = spyOn(_userService, "getAllUsers").and.callFake(
+    //   function() {
+    //     return null;
+    //   }
+    // );
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("spyOn UserService getAllUsers() method", () => {
+    component.ngOnInit();
+    expect(_userServiceSpy).toHaveBeenCalled();
+    expect(component.users).toEqual(users);
+  });
+
+  //.and.callFake test case for user service spy
+
+  // it("getAllUsers() Fake Spy implementation", () => {
+  //   component.ngOnInit();
+  //   expect(_userServiceSpy).toHaveBeenCalled();
+  //   expect(component.users).toEqual(null);
+  // });
 });
